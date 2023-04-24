4f9d07e2fcc24baabcf677304ff280608c22fefc
DatPhit <datvg167@gmail.com>
2023-04-24 15:49:00 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const { log } = require('console');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(
     express.urlencoded({
         extended: true,
@@ -24,22 +26,8 @@ app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.get('/search', (req, res) => {
-    res.render('search');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.render('search');
-});
+// Route init
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on http://localhost:${port}`);
