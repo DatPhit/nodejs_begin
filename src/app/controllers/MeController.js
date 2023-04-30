a7aa373189f9874b852318da944ee584583ba5c1
DatPhit <datvg167@gmail.com>
2023-04-30 10:31:06 +0700

    [CRUD] Update course

---
+++ src/app/controllers/MeController.js
@@ -0,0 +1,14 @@
+const Course = require('../models/Courses');
+const { multipleMongooseToObject } = require('../../util/monggoose');
+class MeController {
+    // [GET] /me/stored/courses
+    storedCourses(req, res, next) {
+        Course.find({})
+            .then((courses) =>
+                res.render('me/stored-courses', { courses: multipleMongooseToObject(courses) }),
+            )
+            .catch(next);
+    }
+}
+
+module.exports = new MeController();
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
 const { log } = require('console');
+
+const methodOverride = require('method-override');
+
 const app = express();
 const port = 3000;
+
+app.use(methodOverride('_method'));
 
 const route = require('./routes');
 const db = require('./config/db');
@@ -26,7 +31,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
 // Template engine`
-app.engine('.hbs', engine({ extname: '.hbs' }));
+app.engine(
+    '.hbs',
+    engine({
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
+    }),
+);
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
